20241b9425dfd30f7ed4bc2d2c8ec66381e835aa
Reis <aglemar.reis@gmail.com>
2024-10-24 09:45:10 -0300

    inicio

---
+++ src/test/java/edu/reis/apiTreino/ApiTreinoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.reis.apiTreino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTreinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
