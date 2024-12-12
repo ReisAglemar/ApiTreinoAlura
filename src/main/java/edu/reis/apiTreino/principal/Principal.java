9e6e853246b84645ce77587d56ee563f94f61825
Reis <aglemar.reis@gmail.com>
2024-12-12 11:27:21 -0300

    Adicionei busca de episodios

--- src/main/java/edu/reis/apiTreino/principal/Principal.java
+++ src/main/java/edu/reis/apiTreino/principal/Principal.java
@@ -10,6 +10,7 @@ import edu.reis.apiTreino.service.ConsumoAPIOmdb;
 import edu.reis.apiTreino.service.ConverteJsonClasse;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -33,31 +34,33 @@ public class Principal {
         this.repository = repository;
     }
 
-    public void menu() {
+    //carrega uma lista dados de seções anteriores
+    private void carregaListaInicialSeries() {
+        seriesObjeto = repository.findAll();
+    }
 
-        carregaListaSeries();
+    public void menu() {
+        carregaListaInicialSeries();
 
         while (true) {
             String menu = """
                     
                     Escolha Uma Opção
                     
-                    1- Buscar e Salvar Série no DB (OK).
+                    1- Buscar e Salvar Série.
                     
-                    2- Buscar Episódios de Uma Série no DB(here).
+                    2- Buscar e Salvar Episódios.
                     
-                    3- Ver os Melhores Episódios de Uma Série.
+                    3- Ver os Melhores Episódios de Uma Série(NOT).
                     
                     4- Listar Séries Salvas no DB.
                     
-                    5- Listar Episódios Buscados.
+                    5- Listar Episódios Buscados(NOT).
                     
                     0- Para Sair.
                     
                     """;
-
             System.out.println(menu);
-
 
             try {
                 int opcao = Integer.parseInt(SCANNER.nextLine());
@@ -97,7 +100,6 @@ public class Principal {
                 System.out.println("ERRO: " + e.getMessage());
             }
         }
-
     }
 
     private void insiraTitulo() {
@@ -121,7 +123,6 @@ public class Principal {
         } catch (Exception e) {
             return "\nErro: " + e.getMessage();
         }
-
         return "\nSérie Salva Com Sucesso!\n";
     }
 
@@ -135,8 +136,6 @@ public class Principal {
 
     private void buscaTodosEpisodio() {
 
-        System.out.println(!seriesObjeto.isEmpty());
-
         if (!seriesObjeto.isEmpty()) {
             listaSerieSalvasDb();
             System.out.println("\nEscolha Uma Série Disponível");
@@ -146,9 +145,23 @@ public class Principal {
                     .filter(s -> s.getTitulo().toLowerCase().contains(escolha.toLowerCase()))
                     .findFirst();
 
-            if (isSerieEncontrada.isPresent()) {
-                System.out.println("presente");
+            var ModeloSeriePessoal = isSerieEncontrada.get();
 
+            if (isSerieEncontrada.isPresent()) {
+                for (int i = 1; i <= ModeloSeriePessoal.getTemporadas(); i++) {
+                    this.linkApi = DOMINIO + escolha.replace(" ", "+") + SEASON + i + API_KEY;
+                    this.json = CONSUMO_OMDB.obterConsumo(linkApi);
+                    ListaEpisodio listaEpisodio = CONVERSOR.converteTipos(json, ListaEpisodio.class);
+                    episodios.add(listaEpisodio);
+                }
+
+                episodiosObjeto = episodios.stream()
+                        .flatMap(e -> e.episodios().stream()
+                                .map(o -> new ModeloEpisodioPessoal(e.temporada(), e.nomeSerie(), o)))
+                        .collect(Collectors.toList());
+
+                ModeloSeriePessoal.setEpisodios(episodiosObjeto);
+                repository.save(ModeloSeriePessoal);
             } else {
                 System.out.println("\nA Série Escolhida Não Existe no DB");
                 System.out.println("\nVocê Escolheu: " + escolha);
@@ -156,20 +169,6 @@ public class Principal {
         } else {
             System.out.println("Você Deve Buscar Uma Série Primeiro");
         }
-
-
-//        for (int i = 1; i <= Integer.parseInt(busca.temporadas()); i++) {
-//
-//            this.linkApi = DOMINIO + titulo.replace(" ", "+") + SEASON + i + API_KEY;
-//            this.json = CONSUMO_OMDB.obterConsumo(linkApi);
-//            ListaEpisodio listaEpisodio = CONVERSOR.converteTipos(json, ListaEpisodio.class);
-//            episodios.add(listaEpisodio);
-//        }
-//
-//        episodiosObjeto = episodios.stream()
-//                .flatMap(e -> e.episodios().stream()
-//                        .map(o -> new ModeloEpisodioPessoal(e.temporada(), e.nomeSerie(), o)))
-//                .collect(Collectors.toList());
     }
 
     private void melhoresEpisodios() {
@@ -185,12 +184,7 @@ public class Principal {
                     .forEach(System.out::println);
             return;
         }
-
         System.out.println("Lista vazia! Faça Uma Busca de Episódios");
-    }
-
-    private void carregaListaSeries() {
-        seriesObjeto = repository.findAll();
     }
 
     private void listaSerieSalvasDb() {
