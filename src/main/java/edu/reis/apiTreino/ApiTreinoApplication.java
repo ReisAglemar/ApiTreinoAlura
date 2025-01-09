2b7c7bc3a286e4bda9d97d36d86edcce50baa0ad
Reis <aglemar.reis@gmail.com>
2025-01-09 11:13:57 -0300

    feat: fiz os ajustes para aplicar na web

--- src/main/java/edu/reis/apiTreino/ApiTreinoApplication.java
+++ src/main/java/edu/reis/apiTreino/ApiTreinoApplication.java
@@ -1,39 +1,13 @@
 package edu.reis.apiTreino;
 
-import edu.reis.apiTreino.principal.Principal;
-import edu.reis.apiTreino.repository.ModeloEpisodioPessoalRepository;
-import edu.reis.apiTreino.repository.ModeloSeriePessoalRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.info.ProjectInfoAutoConfiguration;
 
 @SpringBootApplication
-public class ApiTreinoApplication implements CommandLineRunner {
-
-    @Autowired
-    private ModeloSeriePessoalRepository serieRepository;
-
-    @Autowired
-    private ModeloEpisodioPessoalRepository episodioRepository;
-
-    private final ProjectInfoAutoConfiguration projectInfoAutoConfiguration;
-
-    public ApiTreinoApplication(ProjectInfoAutoConfiguration projectInfoAutoConfiguration) {
-        this.projectInfoAutoConfiguration = projectInfoAutoConfiguration;
-    }
+public class ApiTreinoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiTreinoApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) {
-
-        Principal principal = new Principal(serieRepository, episodioRepository);
-        principal.menu();
-
     }
 }
 
