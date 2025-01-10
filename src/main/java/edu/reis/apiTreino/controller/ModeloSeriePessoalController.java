2d782ccefb8c670d043f1dc8eebc17317e54444d
Reis <aglemar.reis@gmail.com>
2025-01-10 11:00:53 -0300

    fix: ajuste de responsabilidades do controller

--- src/main/java/edu/reis/apiTreino/controller/ModeloSeriePessoalController.java
+++ src/main/java/edu/reis/apiTreino/controller/ModeloSeriePessoalController.java
@@ -1,13 +1,21 @@
 package edu.reis.apiTreino.controller;
 
+import edu.reis.apiTreino.dto.ModeloSeriePessoalDto;
+import edu.reis.apiTreino.service.ModeloSeriePessoalService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ModeloSeriePessoalController {
 
+    @Autowired
+    private ModeloSeriePessoalService service;
+
     @GetMapping("/series")
-    public String obtemSeries() {
-        return "Tu é monstro!!!!";
+    public List<ModeloSeriePessoalDto> obtemModeloSeriePessoal() {
+        return service.buscarSeriePessoal();
     }
 }
