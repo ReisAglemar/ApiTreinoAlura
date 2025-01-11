df0d32f4eac11763932a45742961991bf93d69e7
Reis <aglemar.reis@gmail.com>
2025-01-11 10:43:45 -0300

    feat: criei novas rotas

--- src/main/java/edu/reis/apiTreino/controller/ModeloSeriePessoalController.java
+++ src/main/java/edu/reis/apiTreino/controller/ModeloSeriePessoalController.java
@@ -4,18 +4,31 @@ import edu.reis.apiTreino.dto.ModeloSeriePessoalDto;
 import edu.reis.apiTreino.service.ModeloSeriePessoalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/series")
 public class ModeloSeriePessoalController {
 
     @Autowired
     private ModeloSeriePessoalService service;
 
-    @GetMapping("/series")
+    @GetMapping
     public List<ModeloSeriePessoalDto> obtemModeloSeriePessoal() {
         return service.buscarSeriePessoal();
     }
+
+    @GetMapping("/top5")
+    public List<ModeloSeriePessoalDto> obtemModeloSeriePessoalTop() {
+        return service.buscarMelhorSeriePessoal();
+    }
+
+    @GetMapping("/lancamentos")
+    public List<ModeloSeriePessoalDto> lancamentos() {
+        return service.buscarSeriePessoalLancamentos();
+    }
+
 }
